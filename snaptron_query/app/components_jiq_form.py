e68e3d29c92ad3210f8ed85e59476f5c5433a17f
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-01-29 14:21:00 -0500

    adding string to global file - working throught the files

--- snaptron_query/app/components_jiq_form.py
+++ snaptron_query/app/components_jiq_form.py
@@ -8,17 +8,20 @@ from dash import html, dcc
 from dash_iconify import DashIconify
 
 import inline_styles
+import global_strings
 
 # TODO: GLOBAL: move text into global strings
 
 """
-    Dropdown component
+    Wrapper function to retrieve the dropdown component based on the style
 """
+
+
 def get_dropdown_compilation(component_style):
-    string = 'Compilation'
-    # TODO: GLOBAL: compilation list
-    data_list = ['srav3h', 'gtexv2', 'srav1m', 'tcaga2']
-    placeholder = 'Select a compilation'
+    # component_style='dbc'
+    string = global_strings.drop_compilation
+    data_list = global_strings.compilation_list
+    placeholder = global_strings.drop_compilation_placeholder
     dropdown_id = 'id-input-compilation'
 
     # TODO: check whether default is wanted or have client proactively select
@@ -58,12 +61,14 @@ def get_dropdown_compilation(component_style):
 
 
 """
-    Buttons related to the JIQ form
+    Wrapper function to retrieve the button component based on the style
 """
+
+
 def get_button_add_junction(component_style):
     # search here for icon: https://icon-sets.iconify.design/
     add_icon = DashIconify(icon="ic:round-add-box")
-    string = 'Add Junction'
+    string = global_strings.button_add_junction
     button_id = 'id-button-add-more-junctions'
     if component_style == 'dmc':
         return dmc.Button(
@@ -84,8 +89,13 @@ def get_button_add_junction(component_style):
         )
 
 
+"""
+    Wrapper function to retrieve the button component based on the style
+"""
+
+
 def get_button_generate_results(component_style):
-    string = 'Generate Results'
+    string = global_strings.button_run
     button_id = 'id-button-generate-results'
     if component_style == 'dmc':
         return dmc.Button(
@@ -130,7 +140,7 @@ def get_button_generate_results(component_style):
 
 
 """
-    Text boxes that are in the JIQ form only
+    Wrapper function to retrieve the texted boxes used in the JIQ query based on the style only
 """
 def get_input(component_style, input_placeholder, input_id):
     # debounce:
@@ -155,55 +165,63 @@ def get_input(component_style, input_placeholder, input_id):
         )
 
 
+"""
+    Wrapper functions to dynamically create input textbox components given their id and style
+"""
+
+
 def get_input_chrom(component_style):
-    input_placeholder = 'ex: 19'
+    input_placeholder = global_strings.input_chr_placeholder
     input_id = 'id-input-chromosome'
     return get_input(component_style, input_placeholder, input_id)
 
 
 def get_input_inc_junction(component_style):
-    input_placeholder = 'ex: 4491836-4492014'
+    input_placeholder = global_strings.input_inc_placeholder
     input_id = 'id-input-inc-junc'
     return get_input(component_style, input_placeholder, input_id)
 
 
 def get_input_exc_junction(component_style):
-    input_placeholder = 'ex: 4491836-4493702'
+    input_placeholder = global_strings.input_exc_placeholder
     input_id = 'id-input-exc-junc'
     return get_input(component_style, input_placeholder, input_id)
 
 
-"""
-    Texts that are in the JIQ form only
-   
-"""
-
-# TODO: GLOBAL: move text into global strings
 def get_text(component_style, string):
+    """
+        Wrapper function to retrieve the text used in the JIQ query based on the style only
+    """
     if component_style == 'dmc':
         return dmc.Text(string, weight=500, size="sm")  # semi bold
     elif component_style == 'dcc':
         return html.Label(string)
 
 
+"""
+    Functions Below: Wrapper functions to to dynamically create text components given their string, and style
+"""
+
+
 def get_text_chromosome(component_style):
-    string = 'Chromosome'
+    string = global_strings.input_chr_txt
     return get_text(component_style, string)
 
 
 def get_text_inclusion_junction(component_style):
-    string = 'Inclusion Junction'
+    string = global_strings.input_inc_txt
     return get_text(component_style, string)
 
 
 def get_text_exclusion_junction(component_style):
-    string = 'Exclusion Junction'
+    string = global_strings.input_exc_txt
     return get_text(component_style, string)
 
 
 def get_text_junction(component_style):
     # the size of this is bigger, may change it to sm later
-    string = 'Junction 1'
+    # TODO: MultiJunction query: id fields for the text boxes need to be generated dynamically
+    string = global_strings.input_junction_txt_list[0]
     if component_style == 'dmc':
         return dmc.Text(string, weight=500, size="md")
     elif component_style == 'dcc':
--- snaptron_query/app/graphs.py
+++ snaptron_query/app/graphs.py
@@ -4,18 +4,20 @@
 """
 
 import plotly.express as px
+import global_strings
 
 
-"""
-    Wrapper for plotly express histogram given a df
-    Wrapping in a function for clarity
-"""
 def get_histogram(df):
-    # fig.update traces using below
-    # https://plotly.com/python/reference/histogram/
-    # https://plotly.com/python/histograms/
+    """
+        Wrapper for plotly express histogram given a df
+        Wrapping in a function for clarity
+
+        fig.update traces using below
+        https://plotly.com/python/reference/histogram/
+        https://plotly.com/python/histograms/
+    """
     fig = px.histogram(df,
-                       x='psi',
+                       x=global_strings.table_jiq_col_psi,
                        nbins=50,
                        # title='Histogram of PSI',
                        # labels={'rail_id': 'Rail ID'},
@@ -24,23 +26,26 @@ def get_histogram(df):
                        # color='darkblue',
                        # histnorm='percent' or probability density
                        )
-    fig.update_layout(title='<b>PSI Histogram</b>', title_x=0.5)
+
+    fig.update_layout(title=f'<b>{global_strings.histogram_title}</b>', title_x=0.5)
     fig.update_traces(marker_color='darkblue')
     return fig
 
 
-"""
-    Wrapper for plotly express box plot given a df
-    Wrapping in a function for clarity
-"""
 def get_box_plot(df):
-    # https://plotly.com/python/box-plots/
-    # https://plotly.com/python-api-reference/generated/plotly.express.box
+    """
+        Wrapper for plotly express box plot given a df
+        Wrapping in a function for clarity
+
+        https://plotly.com/python/box-plots/
+        https://plotly.com/python-api-reference/generated/plotly.express.box
+        https://plotly.com/python/reference/box/
+    """
     fig = px.box(df,
-                 y='psi',
-                 hover_data=['rail_id'],
+                 y=global_strings.table_jiq_col_psi,
+                 hover_data=[global_strings.snaptron_col_rail_id],
                  # hover_name='Rail ID',
-                 labels={'rail_id': 'Rail ID'},
+                 labels={global_strings.snaptron_col_rail_id: global_strings.boxplot_label},
                  # color='psi',
                  # Request to not snap with table changes.
                  # If provided, overrides auto-scaling on the y-axis in cartesian coordinates.
@@ -50,14 +55,12 @@ def get_box_plot(df):
                  # title="Box plot of PSI",
                  )
 
-    # https://plotly.com/python/reference/box/
     # fig.update_traces(quartilemethod="exclusive")  # or "inclusive", or "linear" by default
     # fig.update_traces(pointpos=-1.8)
     # fig.update_traces(boxpoints='all')#Type:( "all" | "outliers" (defualt) | "suspectedoutliers" | False )
     # fig.update_traces(jitter=0.3)# add some jitter for a better separation between points
     fig.update_traces(marker_color='darkblue', line_color='royalblue')
-    # title='<b>Bold</b>
-    fig.update_layout(title='<b>PSI Box plot</b>', title_x=0.5)
+    fig.update_layout(title=f'<b>{global_strings.box_plot_title}</b>', title_x=0.5)
     # fig.update_layout(margin = dict(l=10, r=10, t=5, b=5),paper_bgcolor="LightSteelBlue")
 
     return fig
@@ -88,14 +91,16 @@ def get_junction_query_column_def():
     #      },
     # ]
 
+    #TODO: add filter params reset to the psi column, confirm with PI
+
     # columns=['rail_id', 'external_id', 'study', 'inc', 'exc', 'total', 'psi'])
-    columnDefs = [
+    column_defs = [
         {"field": 'rail_id', "headerName": "Rail ID", "filter": "agNumberColumnFilter", },
-        {"field": 'external_id', "headerName": "External IDD"},
+        {"field": 'external_id', "headerName": "External ID"},
         {"field": 'study', "headerName": "Study"},
         {"field": 'inc', "headerName": "Inclusion Count", "filter": "agNumberColumnFilter", },
         {"field": 'exc', "headerName": "Exclusion Count", "filter": "agNumberColumnFilter", },
         {"field": 'total', "headerName": "Total Count", "filter": "agNumberColumnFilter", },
         {"field": 'psi', "headerName": "PSI", "filter": "agNumberColumnFilter", 'initialSort': 'desc'},
     ]
-    return columnDefs
+    return column_defs
