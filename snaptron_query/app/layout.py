55681c26b8271b562849228d37abe6759582e33d
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-02-05 08:57:15 -0500

    pruned files

--- snaptron_query/app/layout.py
+++ snaptron_query/app/layout.py
@@ -4,82 +4,51 @@
 """
 
 import dash_bootstrap_components as dbc
-from dash import html, dcc
-
-import inline_styles as styles
+from dash import html
 import layout_jiq
 import global_strings
 
-"""
-    This is the component at the top of the page. It can be
-"""
-jumbotron = html.Div(
-    dbc.Container(
+
+def get_navbar_top():
+    """
+        This is the component at the top of the page. It can be
+    """
+    return html.Div(
+        dbc.Container(
+            [
+                html.P("Snaptron Custom Query"),
+            ],
+            fluid=True,
+            className="display-4",
+        ),
+        style={'box-shadow': "0 5px 5px -5px #333"},
+        className="bg-primary bg-gradient text-light py-4 text-center fs-1 fw-light",
+    )
+
+
+def get_tabs():
+    """
+        This layout puts the user queries in horizontal tab format.
+        Style: Bootstrap
+        Pros: gives the form layout more room
+        Cons: The aesthetics
+    """
+    tabs = dbc.Tabs(
         [
-            html.P("Snaptron Custom Query"),
-            # html.Hr(className="my-2"),
+            # Junction Inclusion Query Tab
+            dbc.Tab(
+                layout_jiq.get_layout_junction_inclusion(),
+                label=global_strings.tab_jiq,
+                tab_id="id-tab-horizontal-bootstrap-jiq",
+            ),
+
+            # Gene Expression Query Tab
+            dbc.Tab(
+                label=global_strings.tab_geq,
+                tab_id="id-tab-horizontal-bootstrap-geq",
+            ),
         ],
-        fluid=True,
-        className="display-4",  # py-6, display-4 is bigger
-    ),
-    style={'box-shadow': "0 5px 5px -5px #333"},
-    className="bg-primary bg-gradient text-light py-4 text-center fs-1 fw-light",  # bg-secondary
-)
-
-"""
-    This layout puts the user queries in horizontal tab format.
-    Style: Bootstrap
-    Pros: gives the form layout more room
-    Cons: The aesthetics
-"""
-tab_horizontal_bootstrap = dbc.Tabs(
-    [
-        # Junction Inclusion Query Tab
-        dbc.Tab(
-            layout_jiq.get_layout_junction_inclusion(),
-            label=global_strings.tab_jiq,
-            tab_id="id-tab-horizontal-bootstrap-jiq",
-        ),
-
-        # Gene Expression Query Tab
-        dbc.Tab(
-            label=global_strings.tab_geq,
-            tab_id="id-tab-horizontal-bootstrap-geq",
-        ),
-    ],
-    # id="id-tabs",
-    active_tab="id-tab-horizontal-bootstrap-jiq",
-    className="dbc nav-fill",  # Use Bootstrap's nav-fill class to fill the tab_horizontal_bootstrap space
-)
-
-"""
-    This layout puts the user queries in vertical tab format.
-    Style: DCC tabs
-    Pros: gives the form layout more room
-    Cons: The aesthetics
-"""
-tab_horizontal_styled = dcc.Tabs(
-    children=[
-        dcc.Tab(
-            layout_jiq.get_layout_junction_inclusion(),
-            label=global_strings.tab_jiq,
-            value='jiq',
-            style=styles.horizontal_tab,
-            selected_style=styles.horizontal_tab_selected,
-            # selected_className='custom-tab--selected'
-        ),
-        dcc.Tab(  # TODO: Gene expression layout here
-            label=global_strings.tab_geq,
-            value='geq',
-            style=styles.horizontal_tab,
-            selected_style=styles.horizontal_tab_selected,
-            # className='bg-primary',
-            # selected_className='bg-light'
-        ),
-    ],
-    # style=custom_inline_styles.horizontal_tabs,
-    # className='custom-tabs',
-    # className="dbc", #uncomment this to get the dbc theme applied to the tabs
-    id='tabs',
-    value='jiq',  # active tab
-)
+        active_tab="id-tab-horizontal-bootstrap-jiq",
+        className="dbc nav-fill",  # Use Bootstrap's nav-fill class to fill the tab_horizontal_bootstrap space
+    )
+    return tabs
--- snaptron_query/app/main_dash_app.py
+++ snaptron_query/app/main_dash_app.py
@@ -6,48 +6,36 @@ from dash.exceptions import PreventUpdate
 
 import graphs
 import layout
-from query_junction_inclusion import JunctionInclusionQueryManager as jiq_mgr
-from snaptron_client import SnaptronClientManager
+import global_strings
 
 # Initialize the app
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 bs_cdn = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 app = Dash(__name__,
-           suppress_callback_exceptions=True,
            external_stylesheets=[dbc.themes.SANDSTONE,
                                  dbc_css,
-                                 # bs_cdn,
-                                 # dbc.icons.BOOTSTRAP
                                  ])
 
 # this is the main layout of the page with all tabs
 app.layout = dbc.Container(
     [
         # app uses a dcc.store to store information needed between components from callbacks
-        # this is stored in the user's browser session
-        # data must be in some JSON format
-        dcc.Store(id="id-store-info", storage_type='memory'),
-        # dcc.Store(id="id-store-info2", storage_type='memory'),
-        dcc.Store(id="id-store-jiq-df", storage_type='memory'),
+
+        dcc.Store(id="id-store-info"),
+        dcc.Store(id="id-store-jiq-df"),
 
         # navbar,# Top row with titles and all
-        layout.jumbotron,
+        layout.get_navbar_top(),
 
         # Next row is are the tabs and their content
         dmc.Space(h=30),
-        layout.tab_horizontal_bootstrap,
-        # other options
-        # layout.tab_horizontal_styled,
-        # layout.tab_vertical_styled,
-        # layout_jiq.junction_inclusion_query_layout,
+        layout.get_tabs(),
+
 
         dmc.Space(h=30),
-        html.Div(id="id-log-content",
-                 # style={'display:none'}
-                 ),
+        html.Div(id="id-log-content"),
     ],
-    # fluid=True, # this will make the page use full screen width
-    # className='container-fluid'
+    fluid=True,  # this will make the page use full screen width
 )
 
 
@@ -59,69 +47,57 @@ app.layout = dbc.Container(
     Input(component_id="id-input-inc-junc", component_property="value"),
     Input(component_id="id-input-exc-junc", component_property="value"),
     Input('id-store-info', 'data'),
-    # allow_duplicate=True,
     prevent_initial_call=True
 )
 def on_button_click_gen_results(n_clicks, compilation, inc, exc, datasets):
-    # TODO this function gets called with every input change, not just the button click,
-    #  need to narrow to just the button click
-
-    # if n_clicks is None:
-    #     #return no_update
-    #     raise PreventUpdate
-
+    #  this function gets called with every input change, not just the button click
     if not datasets:
         datasets = dict(clicks=0, log='')
-        # data_dict = dict()
 
     old_click = datasets['clicks']
     if n_clicks <= old_click:
         raise PreventUpdate
     else:
+        # ----------------------------------------
+        #       Work In Progress Here
+        # ----------------------------------------
         # Gather the form data and create the URL
         # TODO: hardcode URL for now
-        chromosome_number =19
+        chromosome_number = 19
         host = 'https://snaptron.cs.jhu.edu'
         query_type_string = 'snaptron'  # vs 'genes' for gene expression
         head = f'{host}/{compilation}/{query_type_string}?regions=chr{chromosome_number}' + ':'
-        url = f'{head}{exc}'
-        url = 'https://snaptron.cs.jhu.edu/srav3h/snaptron?regions=chr19:4491836-4493702'
+        url = f'{head}{exc}{inc}'
+        # url = 'https://snaptron.cs.jhu.edu/srav3h/snaptron?regions=chr19:4491836-4493702'
+        # TODO: run the query and return result's data frame here
+        # putting example data for now
+        data = {
+            'rail_id': [1, 2, 3, 4, 5],
+            'external_id': [10, 20, 30, 40, 50],
+            'study': ['AA', 'BB', 'CC', 'DD', 'EE'],
+            'inc': [25, 30, 22, 28, 35],
+            'exc': [25, 30, 22, 28, 35],
+            'total': [25, 30, 22, 28, 35],
+            'psi': [15, 26, 34, 5, 17],
+        }
+        results_df = pd.DataFrame(data)
+        # ----------------------------------------
 
-        (inclusion_start, inclusion_end) = inc.split('-')
-        (exclusion_start, exclusion_end) = exc.split('-')
-
-        # RUN the URL and get results back from SNAPTRON
-        sqm = SnaptronClientManager(url)
-        df = sqm.get_query_results_dataframe()
-
-        # Process the results returned and run the Junction Inclusion Query
-        jqm = jiq_mgr(int(exclusion_start),
-                      int(exclusion_end),
-                      int(inclusion_start),
-                      int(inclusion_end))
-
-        results_df = jqm.run_junction_inclusion_query(df)
-
-        # prepare to put into session storage
         data_dict = results_df.to_dict(orient='list')
-
         # keep track of any log needed
-        nl = '\n'
-        log_msg = f'Button clicked {n_clicks} times.{nl} URL={url[0]}.'
+        log_msg = f'Click= {n_clicks}-URL={url[0]}'
         datasets['log'] = log_msg
-        datasets['clicks'] = n_clicks  # update the clicks info for reducing callbacks
+        datasets['clicks'] = n_clicks
 
     return datasets, data_dict
 
 
 @app.callback(
-    # Output('id-table', 'children'),
     Output('id-ag-grid', 'rowData'),
     Output('id-ag-grid', 'columnDefs'),
     Output('id-card-table', 'style'),
     Input('id-store-jiq-df', 'data'),
     Input('id-card-table', 'style'),
-    # allow_duplicate=True,
     prevent_initial_call=True
 )
 def update_table(data_from_store, current_style):
@@ -132,6 +108,7 @@ def update_table(data_from_store, current_style):
     df = pd.DataFrame(data_from_store)
     df['psi'] = df['psi'].astype('float')
     row_data = df.to_dict("records")
+
     # Set the columnDefs for the ag-grid
     column_defs = graphs.get_junction_query_column_def()
 
@@ -139,20 +116,21 @@ def update_table(data_from_store, current_style):
     # set component visibility
     current_style['display'] = 'block'
 
-    # parentheses must be here
+    # parentheses must be here, dash does not like it without it
     return (row_data, column_defs, current_style)  # grid
 
 
 @app.callback(
     Output('id-histogram', 'figure'),
-    Output('id-log-content', 'children'),
     Output('id-box-plot', 'figure'),
-
     Input('id-ag-grid', 'rowData'),
     Input('id-ag-grid', 'virtualRowData'),
     Input('id-switch-lock-with-table', 'value')
 )
 def update_charts(row_data_from_table, filtered_row_data_from_table, lock_graph_data_with_table):
+    """
+        Given the table data as input, it will update the relative graphs
+    """
     if not row_data_from_table or not filtered_row_data_from_table:
         raise PreventUpdate
 
@@ -161,23 +139,10 @@ def update_charts(row_data_from_table, filtered_row_data_from_table, lock_graph_
     else:
         df = pd.DataFrame(row_data_from_table)
 
-    df['psi'] = df['psi'].astype('float')
+    df[global_strings.table_jiq_col_psi] = df[global_strings.table_jiq_col_psi].astype('float')
     histogram = graphs.get_histogram(df)
     box_plot = graphs.get_box_plot(df)
-    log = f'testing'
-    return histogram, log, box_plot
-
-
-# @app.callback(
-#     Output('log-content', 'children'),
-#     Input('id-store', 'data'),
-#     # allow_duplicate=True,
-#     prevent_initial_call=True
-# )
-# def update_log(data):
-#     if data:
-#         # datasets = json.loads(data)
-#         return data.get('log', '')
+    return histogram, box_plot
 
 
 # Run the app
