792f04c9cf1ef07109da724ba10966b0c1052a6b
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-02-06 11:10:31 -0500

    fixed strings issue in the compilation list so code can run

--- snaptron_query/app/global_strings.py
+++ snaptron_query/app/global_strings.py
@@ -2,13 +2,19 @@
     General Strings
 """
 
-tab_jiq = "Junction Inclusion Query"
+tab_jiq = "Junction PSI Query"
 tab_geq = "Gene Expression Query"
 
 """
     Strings used in the Junction Inclusion Query Form
 """
-compilation_list = ['srav3h', 'gtexv2', 'srav1m', 'tcaga2']
+compilation_names = ['SRAv3h', 'GTEXv2', 'TCGAv2', 'SRAv1m']
+compilation_names_dict = {
+    compilation_names[0]: f'human {compilation_names[0]}',
+    compilation_names[1]: f'human {compilation_names[1]}',
+    compilation_names[2]: f'human {compilation_names[2]}',
+    compilation_names[3]: f'mouse {compilation_names[3]}',
+}
 drop_compilation = 'Select the organism of interest'
 drop_compilation_placeholder = 'Select a compilation'
 jiq_form_title = 'Junction Information'
@@ -48,6 +54,19 @@ srav3h_meta_data_required_list = [snaptron_col_rail_id,
                                   snaptron_col_study_title,
                                   snaptron_col_library_layout
                                   ]
+srav1m_meta_data_required_list = srav3h_meta_data_required_list  # SRA mouse and SRA human are similar
+
+tcgav2_meta_data_required_list = ["rail_id","tcga_barcode", "study","gdc_cases.project.name",
+                                  "gdc_cases.project.primary_site", "cgc_sample_sample_type",
+                                  "gdc_state","gdc_cases.demographic.race",
+                                  "gdc_cases.demographic.ethnicity",
+                                  "gdc_cases.diagnoses.tumor_stage",
+                                  "gdc_cases.diagnoses.vital_status","gdc_cases.samples.oct_embedded",
+                                  "gdc_cases.samples.is_ffpe","gdc_cases.samples.sample_type",
+                                  "cgc_sample_country_of_sample_procurement","cgc_case_tumor_status",
+                                  "cgc_drug_therapy_pharmaceutical_therapy_type","cgc_follow_up_tumor_status"
+                                  ]
+gtexv2_meta_data_required_list = ["rail_id","run_acc", "study","SEX","AGE", "SAMPID","SMTS","SMTSD"]
 
 # columns=['rail_id', 'external_id', 'study', 'inc', 'exc', 'total', 'psi'])
 table_jiq_col_inc = 'inc'
