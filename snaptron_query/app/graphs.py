7cab60a0484d82b8bc4cb51631f503a3b9e034de
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-03-07 10:18:28 -0500

    upper case PSI per request

--- snaptron_query/app/graphs.py
+++ snaptron_query/app/graphs.py
@@ -44,7 +44,8 @@ def get_box_plot(df, log_psi_values, violin_overlay):
 
     else:
         fig = px.box(df, y=y_values, hover_data=[global_strings.snaptron_col_rail_id],
-                     labels={global_strings.snaptron_col_rail_id: global_strings.boxplot_label},
+                     labels={global_strings.snaptron_col_rail_id: global_strings.boxplot_label,
+                             global_strings.table_jiq_col_psi:global_strings.table_jiq_col_psi.upper()},
                      # Request to not snap with table changes.
                      # If provided, overrides auto-scaling on the y-axis in cartesian coordinates.
                      range_y=[0, 110],
@@ -56,7 +57,7 @@ def get_box_plot(df, log_psi_values, violin_overlay):
 
     # update the y axis title if log switch is on
     if log_psi_values:
-        fig.update_yaxes(title_text='Log₂(psi)')
+        fig.update_yaxes(title_text='Log₂(PSI)')
 
     fig.update_layout(title=f'<b>{global_strings.box_plot_title}</b>', title_x=0.5)
 
