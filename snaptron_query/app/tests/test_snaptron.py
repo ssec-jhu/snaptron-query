2194b991fc48ec78ecb53ea5a975f5bc7cd68633
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-05-15 14:10:59 -0400

    adding comments to the tests

--- snaptron_query/app/tests/test_snaptron.py
+++ snaptron_query/app/tests/test_snaptron.py
@@ -7,6 +7,7 @@ from snaptron_query.app import snaptron_client as sc, global_strings as gs
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 
+# skipping test on GITHUB as these tests are tests that run against the snaptron API itself and does a http request
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipping test on Github")
 @pytest.mark.parametrize('compilation, region, query_mode',
                          [(gs.compilation_srav3h, 'chr19:4491836-4493702', 'snaptron'),
@@ -18,6 +19,7 @@ def test_snaptron(compilation, region, query_mode):
     assert not df.empty
 
 
+# skipping test on GITHUB as these tests are tests that run against the snaptron API itself and does a http request
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipping test on Github - this must run locally")
 @pytest.mark.parametrize('human_gene', ['TARDBP', 'ACTL6B', 'AGRN', 'EPB41L4A', 'GPSM2', 'PFKP', 'RANBP1', 'STMN2',
                                         'UNC13A', 'UNC13B', 'SLC24A3', 'IGLON5', 'KALRN', 'MYO18A', 'RSF1', 'SYT7',
@@ -28,6 +30,7 @@ def test_snaptron_gene_human(compilations, human_gene):
         assert not df.empty
 
 
+# skipping test on GITHUB as these tests are tests that run against the snaptron API itself and does a http request
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipping test on Github - this must run locally")
 @pytest.mark.parametrize('human_gene', ['MT1XP1', 'HDGFL2'])
 def test_snaptron_gene_human_missing(compilations, human_gene):
