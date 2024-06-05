2d96ddefdeaf2804de86e8bfdc7b6e3ce106667c
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-06-05 14:08:10 -0400

    updting meta data path dirctory (#51)

--- snaptron_query/app/paths.py
+++ snaptron_query/app/paths.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
 # path to the directory where the metadata for the compilations reside
-meta_data_directory = Path(__file__).parent / 'data/'
+# TODO: this hardcoded path needs to be in a config file outside the container in the future
+# switch to the first line for deployment and
+# switch to the line below it for running locally
+meta_data_directory = Path('/srv/nvme2/snaptron_data/snapmine_samples')
+# meta_data_directory = Path(__file__).parent / 'data/'
+
 
 # specify the file name to the metadata files for each compilation here
 filename_srav3h = 'srav3h_samples.tsv'
