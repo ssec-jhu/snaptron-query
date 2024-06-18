8aa347cd80b30462241210ab5096a017e3b73178
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-06-17 20:03:44 -0400

    formatting files

--- snaptron_query/app/paths.py
+++ snaptron_query/app/paths.py
@@ -4,15 +4,15 @@ from pathlib import Path
 # TODO: this hardcoded path needs to be in a config file outside the container in the future
 # switch to the first line for deployment and
 # switch to the line below it for running locally
-meta_data_directory = Path('/srv/nvme2/snaptron_data/snapmine_samples')
+meta_data_directory = Path("/srv/nvme2/snaptron_data/snapmine_samples")
 # meta_data_directory = Path(__file__).parent / 'data/'
 
 
 # specify the file name to the metadata files for each compilation here
-filename_srav3h = 'srav3h_samples.tsv'
-filename_gtexv2 = 'gtexv2_samples.tsv'
-filename_tcgav2 = 'tcgav2_samples.tsv'
-filename_srav1m = 'srav1m_samples.tsv'
+filename_srav3h = "srav3h_samples.tsv"
+filename_gtexv2 = "gtexv2_samples.tsv"
+filename_tcgav2 = "tcgav2_samples.tsv"
+filename_srav1m = "srav1m_samples.tsv"
 
 
 srav3h_meta = meta_data_directory / filename_srav3h
