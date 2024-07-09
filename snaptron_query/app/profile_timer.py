99ea453f59f3deb886d8264de3f443c8fe59bbf1
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-07-09 10:35:50 -0400

    Custom timer (#108)
    * custom timer
    
    * fixed timer
    
    * custom timer
    
    * custom timer

---
+++ snaptron_query/app/profile_timer.py
@@ -0,0 +1,59 @@
+from timeit import default_timer as timer
+
+from dash import callback_context as ctx
+
+
+class Timer:
+    def __init__(self, function_name=None):
+        self.enabled = False  # Set to True to turn on the timers
+        self.start_time = None
+        self.split_start_time = None
+        self.function_name = function_name
+        self.start()
+
+    def start(self):
+        """
+        starts the timer
+        """
+        if self.enabled:
+            self.start_time = timer()
+            print(f"Start: {self.function_name}")
+
+    def stop(self, msg=None):
+        """
+        stop the timer completely
+        :param msg: provide function name as strings
+        """
+        if self.enabled and self.start_time is not None:
+            # stop the time
+            end_time = timer()
+
+            # if there was a split before it calculate the splits
+            if self.split_start_time:
+                elapsed = end_time - self.split_start_time
+                print(f"\t{msg}: {elapsed:.6f} seconds")
+                ctx.record_timing(msg, elapsed)
+
+            total = end_time - self.start_time
+            print(f"End: {self.function_name}\tTotal time: {total:.6f} seconds")
+            ctx.record_timing(self.function_name, total)
+
+    def split(self, msg=None):
+        """
+        Use this function to create time splits in the main function.
+        The previous split timer will stop and a new split timer will start
+        :param msg: provide function name as strings
+        """
+        if self.enabled:
+            if self.start_time is not None:
+                current_time = timer()
+                if self.split_start_time is None:
+                    # first split
+                    split_time = current_time - self.start_time
+                else:
+                    split_time = current_time - self.split_start_time
+
+                print(f"\t{msg}: {split_time:.6f} seconds")
+                ctx.record_timing(msg, split_time)
+                # start the timer for the next split
+                self.split_start_time = current_time
