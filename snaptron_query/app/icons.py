fa45dac0af1d9bdc7f151f8921c25a64fb300444
Irika Sinha <isinha1@jh.edu>
2025-03-28 11:47:33 -0400

    Resolve #238
    Added requested style changes

--- snaptron_query/app/icons.py
+++ snaptron_query/app/icons.py
@@ -8,7 +8,9 @@ del_box = html.I(DashIconify(icon="mdi:minus-box", height=SMALL, width=SMALL))
 download = html.I(DashIconify(icon="mdi:tray-download", height=MEDIUM, width=MEDIUM))
 reset = html.I(DashIconify(icon="mdi:filter-remove", height=MEDIUM, width=MEDIUM))
 info = html.I(DashIconify(icon="fa6-solid:circle-info", height=SMALL, width=SMALL), style={"color": "var(--bs-info)"})
-caution = html.I(DashIconify(icon="fluent:warning-32-filled", height=SMALL, width=SMALL), style={"color": "#e38e23"})
+caution = html.I(
+    DashIconify(icon="fa6-solid:triangle-exclamation", height=SMALL, width=SMALL), style={"color": "var(--bs-warning)"}
+)
 
 
 def get_lock_opened(icon_id):
